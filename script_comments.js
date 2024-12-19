c6cec94e64b8a80dac8a95ff4383fa5edcea22ec
kabalya <112944418+kabalya@users.noreply.github.com>
2024-12-19 14:24:47 +0300

    fix lab 6

--- script_comments.js
+++ script_comments.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const preloader = document.getElementById('preloader');
     const commentsContainer = document.getElementById('commentsContainer');
     const loadCommentsButton = document.getElementById('loadCommentsButton');
+    const commentTemplate = document.getElementById('commentTemplate');
 
     async function fetchComments() {
         try {
             let response;
             const randomNum = Math.random();
 
-            if (randomNum < 0.5){
+            if (randomNum < 0.5) {
                 response = await fetch('https://jsonplaceholder.typicode.com/comments?_start=100');
-            } else{
+            } else {
                 response = await fetch('https://jsonplaceholder.typicode.com/comments?_end=200');
             }
 
@@ -22,9 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const comments = await response.json();
             renderComments(comments);
         } catch (error) {
-            commentsContainer.innerHTML = `<p>⚠ Что-то пошло не так: ${error.message}</p>`;
+            commentsContainer.innerHTML = `<p>⚠️ Что-то пошло не так: ${error.message}</p>`;
         } finally {
-
             preloader.style.display = 'none';
         }
     }
@@ -32,18 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function renderComments(comments) {
         commentsContainer.innerHTML = '';
         for (const comment of comments) {
-            const commentElem = document.createElement('div');
-            commentElem.classList.add('comment');
-    
-            const nameElem = document.createElement('h4');
-            const textElem = document.createElement('p');
-    
-            nameElem.textContent = `${comment.name} (${comment.email})`;
-            textElem.textContent = comment.body;
-    
-            commentElem.append(nameElem, textElem);
+            const commentElem = commentTemplate.content.cloneNode(true);
+            
+            // Заполнение шаблона данными комментария
+            commentElem.querySelector('h4').textContent = `${comment.name} (${comment.email})`;
+            commentElem.querySelector('p').textContent = comment.body;
+
             commentsContainer.append(commentElem);
-          }
+        }
     }
 
     loadCommentsButton.addEventListener('click', () => {
